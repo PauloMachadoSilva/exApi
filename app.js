ebc4ac64a2ed25321a99c3b3a655d13642422068
Ivan Francisco <ivan.francisco@ext.sinqia.com.br>
2023-02-24 10:12:25 -0300

    commit files

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.send("Página inicial - Celke");
+});
+
+app.get("/teste", async (req, res) => {
+    res.send("Teste");
+});
+
+app.get("/produtos", async (req, res) => {
+    execSQLQuery('SELECT * FROM produtos', res);
+});
+
+app.post("/cadastrar", async (req, res) => {
+    //console.log(req.body);
+
+    await Produtos.create(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            mensagem: "Usuário cadastrado com sucesso!"
+        });
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário não cadastrado com sucesso!"
+        });
+    });
+
+    //res.send("Página cadastrar");
+});
+
+function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection({
+      host     : 'localhost',
+      // port     : XXX,
+      user     : 'sistema_01',
+      password : 'alpha123',
+      database : 'sistema_orcamento'
+    });
+
+    connection.query(sqlQry, (error, results, fields) => {
+        if(error) 
+          res.json(error);
+        else
+          res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+}
+
+
+app.listen(3030, () => {
+    console.log("Servidor iniciado na porta 3030: http://localhost:3030");
+});
