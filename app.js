06ccbab95ec091e43d90c43878c2ae798324f5bd
Ivan Francisco <ivan.francisco@ext.sinqia.com.br>
2023-02-24 12:41:56 -0300

    Enable cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+const cors = require('cors')
+
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/", async (req, res) => {
     res.send("Página inicial - Celke");
